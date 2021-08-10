429a754cbfb6e6168802581906c1cd91db51d7f7
Alexander Ovchinnikov <Alexander.Ovchinnikov@mendix.com>
2021-08-10 14:21:27 +0200

    Add ProxyOptions type
    Add GitProxyOptionsWrapper to generate callbacks and do marshaling

---
+++ LibGit2Sharp/Core/GitProxyOptionsWrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LibGit2Sharp.Core
+{
+    internal class GitProxyOptionsWrapper : IDisposable
+    {
+        public GitProxyOptionsWrapper(ProxyOptions proxyOptions)
+        {
+            GitProxyOptions = BuildFrom(proxyOptions);
+        }
+
+        internal GitProxyOptions GitProxyOptions;
+
+        private GitRemoteCallbacks GitCallbacks;
+
+        private GitProxyOptions BuildFrom(ProxyOptions proxyOptions)
+        {
+            var opts = new GitProxyOptions
+            {
+                Version = 1,
+                Type = GitProxyType.Auto
+            };
+
+            if (proxyOptions == null)
+            {
+                return opts;
+            }
+
+            if (proxyOptions.Url != null)
+            {
+                opts.Url = StrictUtf8Marshaler.FromManaged(proxyOptions.Url);
+                opts.Type = GitProxyType.Specified;
+            }
+
+            var callbacks = new RemoteCallbacks(proxyOptions.CredentialsProvider, proxyOptions.CertificateCheck);
+            GitCallbacks = callbacks.GenerateCallbacks();
+
+            if (GitCallbacks.acquire_credentials != null)
+            {
+                opts.CredentialsCb = GitCallbacks.acquire_credentials;
+            }
+
+            if (GitCallbacks.certificate_check != null)
+            {
+                opts.CertificateCheck = GitCallbacks.certificate_check;
+            }
+
+            return opts;
+        }
+
+        public void Dispose()
+        {
+            EncodingMarshaler.Cleanup(GitProxyOptions.Url);
+            GitProxyOptions.Url = IntPtr.Zero;
+        }
+    }
+}
